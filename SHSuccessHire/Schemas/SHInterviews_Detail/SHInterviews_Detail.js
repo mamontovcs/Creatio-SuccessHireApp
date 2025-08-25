4843e3fbf072665fbb04d3ae7ae52d3e4c875465
Vitalii Mamontov <v.mamontov@creatio.com>
2025-08-25 16:32:13 +0300

    Added sections

---
+++ SHSuccessHire/Schemas/SHInterviews_Detail/SHInterviews_Detail.js
@@ -0,0 +1,8 @@
+define('SHInterviews_Detail', [], function () {
+	return {
+		entitySchemaName: 'SHInterview',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ SHSuccessHire/Schemas/SHInterviews_FormPage/SHInterviews_FormPage.js
@@ -0,0 +1,112 @@
+define("SHInterviews_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "SHName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.SHName",
+					"control": "$SHName",
+					"labelPosition": "auto"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"type": "crt.FileList",
+					"masterRecordColumnValue": "$Id",
+					"recordColumnName": "RecordId",
+					"layoutConfig": {
+						"colSpan": 2,
+						"column": 1,
+						"row": 1,
+						"rowSpan": 6
+					},
+					"items": "$AttachmentList",
+					"primaryColumnName": "AttachmentListDS_Id",
+					"columns": [
+						{
+							"id": "15a21744-689d-4911-ac3c-91136572ae60",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					],
+					"viewType": "gallery",
+					"tileSize": "small"
+				},
+				"parentName": "AttachmentsTabContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"type": "crt.Feed",
+					"feedType": "Record",
+					"primaryColumnValue": "$Id",
+					"cardState": "$CardState",
+					"dataSourceName": "PDS",
+					"entitySchemaName": "SHInterview"
+				},
+				"parentName": "FeedTabContainer",
+				"propertyName": "items",
+				"index": 0
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"SHName": {
+					"modelConfig": {
+						"path": "PDS.SHName"
+					}
+				},
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"PDS": {
+					"type": "crt.EntityDataSource",
+					"config": {
+						"entitySchemaName": "SHInterview"
+					},
+					"scope": "page"
+				},
+				"AttachmentListDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "viewElement",
+					"config": {
+						"entitySchemaName": "SysFile",
+						"attributes": {
+							"Name": {
+								"path": "Name"
+							}
+						}
+					}
+				}
+			},
+			"primaryDataSourceName": "PDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
